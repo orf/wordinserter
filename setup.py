889be83c30d3053d6b5797eb2c494716069a4d67
tom.forbes <tom.forbes@contextis.co.uk>
2016-02-15 16:44:41 +0000

    Add pypiwin32 requirement and MIT license.

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     version='0.6',
     packages=find_packages(),
     url='https://github.com/orf/wordinserter',
-    license='',
+    license='MIT',
 
     author='Tom',
     author_email='tom@tomforb.es',
     description='Render HTML and Markdown to a specific portion of a word document',
-    install_requires=["BeautifulSoup4", "cssutils", 'CommonMark', 'requests', 'webcolors'],
+    install_requires=["BeautifulSoup4", "cssutils", 'CommonMark', 'requests', 'webcolors', 'pypiwin32'],
     include_package_data=True,
     long_description="""\
 Render HTML and Markdown into a word document using win32com.
