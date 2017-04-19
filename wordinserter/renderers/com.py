90eb1d39a04ae03aa2487e6399ed51f318cadfdc
Tom Forbes <tom.forbes@contextis.co.uk>
2017-04-19 12:03:11 +0100

    Pass an explicit id to format() for headings

--- wordinserter/renderers/com.py
+++ wordinserter/renderers/com.py
@@ -127,7 +127,7 @@ class COMRenderer(BaseRenderer):
     @renders(Heading)
     def heading(self, op: Heading):
 
-        with self.style(Style(name="Heading {0}".format(op.level))):
+        with self.style(Style(name="Heading {0}".format(op.level), id=op.id)):
             yield self.new_operations(op.children)
 
     @renders(Style)
