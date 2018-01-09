9843cae050fde9dd2e42fd4ea1fb10e5539e0498
Tom Forbes <tom@tomforb.es>
2018-01-09 00:54:25 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if os.path.exists('README.rst'):
 
 setup(
     name='wordinserter',
-    version='1.0.8',
+    version='1.0.9',
     packages=find_packages(),
     url='https://github.com/orf/wordinserter',
     license='MIT',
